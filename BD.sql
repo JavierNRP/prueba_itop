5eb4c95bbf5b72a47f893d6a3d75e8d731642f75
Javier Nicolas <javiernrp@gmail.com>
2020-09-09 19:55:44 +0100

    BD changes
    Cambio a la base de datos a como entiendo que es y optimizacion

--- BD.sql
+++ BD.sql
@@ -6,37 +6,43 @@ USE BD_prueba_itop;
 
 CREATE TABLE Entity (
 	id INT NOT NULL AUTO_INCREMENT,
-	modulo VARCHAR(30),
 	borrado boolean,
 	fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
 	fecha_modificacion DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
 
-INSERT INTO Entity (modulo, borrado) VALUES ('prueba', false);
+INSERT INTO Entity (borrado) 
+	VALUES (true),(false),(false),(true),(false),(true),(false);
+	-- Ids 1-7 --
 	
+
 CREATE TABLE Potenciales (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL UNIQUE,
 	nombre VARCHAR(30),
 	apellido VARCHAR(30),
-	dni CHAR(9),
+	dni CHAR(9) UNIQUE,
 	estado VARCHAR(30),
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES Entity (id) ON DELETE  CASCADE
 );
 
+
 CREATE TABLE Actividades (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL UNIQUE,
 	nombre VARCHAR(30),
 	descripcion VARCHAR(200),
 	fecha DATETIME,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES Entity (id) ON DELETE  CASCADE
 );
 
+
 CREATE TABLE EntityRel (
-	id INT NOT NULL AUTO_INCREMENT,
-	modulo INT,
-	relid INT,
-	relmodulo INT,
-	PRIMARY KEY (id)
+	pot_id INT NOT NULL,
+	act_id INT NOT NULL,
+	PRIMARY KEY (pot_id, act_id)
+	FOREIGN KEY (pot_id) REFERENCES Potenciales (id) ON DELETE  CASCADE,
+	FOREIGN KEY (act_id) REFERENCES Actividades (id) ON DELETE  CASCADE
 );
 /**/
